ad9173805708a7e3eb0eae51c76fef882a547210
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-05-14 08:43:24 -0300

    Visual Structure - adding carousel using external lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
+'use client'
 import Image from 'next/image'
 import { HomeContainer, Product } from '../styles/pages/home'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+    breakpoints: {
+      '(max-width: 900px)': {
+        slides: {
+          perView: 2,
+          spacing: 48,
+        },
+      },
+      '(max-width: 600px)': {
+        slides: {
+          perView: 1,
+          spacing: 48,
+        },
+      },
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image
           src="/shirts/1.png"
           width={520}
@@ -15,11 +40,11 @@ export default function Home() {
 
         <footer>
           <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
+          <span>R$ 69,90</span>
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image
           src="/shirts/2.png"
           width={520}
@@ -29,8 +54,38 @@ export default function Home() {
         />
 
         <footer>
-          <strong>Camiseta X</strong>
+          <strong>Camiseta y</strong>
           <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src="/shirts/3.png"
+          width={520}
+          height={480}
+          alt="Camiseta X"
+          loading="lazy"
+        />
+
+        <footer>
+          <strong>Camiseta z</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src="/shirts/3.png"
+          width={520}
+          height={480}
+          alt="Camiseta A"
+          loading="lazy"
+        />
+
+        <footer>
+          <strong>Camiseta A</strong>
+          <span>R$ 99,90</span>
         </footer>
       </Product>
     </HomeContainer>
