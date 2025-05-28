83def2dc43ceaffd9091223871b69a53c3b401b1
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-05-28 17:31:31 -0300

    chore(config): replace deprecated images.domains with images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['files.stripe.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
   },
 }
 
