afa2653226ff2892f1e02f285747be0e98cab341
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-05-28 15:42:51 -0300

    SSR transformed into ISR - Easier way

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 100 // ISR Revalidation variable
+
 import ProductsSliderHome from '@/slider/ProductsSliderHome'
 import { stripe } from '../lib/stripe'
 import Stripe from 'stripe'
@@ -8,6 +10,9 @@ async function getProducts() {
     expand: ['data.default_price'],
   })
 
+  const timestamp = new Date().toISOString()
+  console.log(`[ISR] Produtos revalidados em: ${timestamp}`)
+
   return response.data.map((product) => {
     const price = product.default_price as Stripe.Price
 
@@ -16,13 +21,14 @@ async function getProducts() {
       name: product.name,
       imageUrl: product.images[0],
       price: price.unit_amount ? price.unit_amount / 100 : 0,
+      fetchedAt: timestamp, // to see it on client
     }
   })
 }
 
 // Server Component that fetches products and passes them to the Client Component
 export default async function Home() {
-  const products = await getProducts() // Now the fetching is done by the server component
+  const products = await getProducts()
 
-  return <ProductsSliderHome products={products} /> // passing the products to the client component
+  return <ProductsSliderHome products={products} />
 }
